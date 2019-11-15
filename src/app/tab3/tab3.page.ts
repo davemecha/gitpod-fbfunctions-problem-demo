0783faf3f72cec1b9799be88f00e1cd77aa19338
Dave Gööck <dave@goeoeck.net>
2019-11-15 15:42:30 +0000

    add firebase functions

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-tab3',
@@ -6,7 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
+  private test: Subject<boolean> = new Subject();
 
-  constructor() {}
+  constructor() {
+  }
 
 }
